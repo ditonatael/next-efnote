3336d6ca888286bf0a262a929bb26e80aeee54e3
ditonatael <158138590+ditonatael@users.noreply.github.com>
2024-03-24 16:03:43 +0700

    feat: fetch teams data

--- src/app/teams/page.jsx
+++ src/app/teams/page.jsx
@@ -1,4 +1,22 @@
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import React from "react";
+import Image from "next/image";
+
 export default function OurTeams() {
+  const [team, setTeam] = useState([]);
+
+  const onFetchTeam = async () => {
+    const res = await axios.get(`https://randomuser.me/api/?results=6`);
+    console.log(res);
+    setTeam(res.data.results);
+  };
+
+  useEffect(() => {
+    onFetchTeam();
+  }, []);
+
   return (
     <section className="py-6 dark:bg-gray-100 dark:text-gray-800">
       <div className="container flex flex-col items-center justify-center p-4 mx-auto space-y-8 sm:p-10">
@@ -9,60 +27,28 @@ export default function OurTeams() {
           The talented people behind the scene
         </p>
         <div className="flex flex-row flex-wrap-reverse justify-center">
-          <div className="flex flex-col justify-center m-8 text-center">
-            <img
-              alt=""
-              className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500"
-              src="https://source.unsplash.com/100x100/?portrait?0"
-            />
-            <p className="text-xl font-semibold leading-tight">Leroy Jenkins</p>
-            <p className="dark:text-gray-600">Visual Designer</p>
-          </div>
-          <div className="flex flex-col justify-center m-8 text-center">
-            <img
-              alt=""
-              className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500"
-              src="https://source.unsplash.com/100x100/?portrait?1"
-            />
-            <p className="text-xl font-semibold leading-tight">Leroy Jenkins</p>
-            <p className="dark:text-gray-600">Visual Designer</p>
-          </div>
-          <div className="flex flex-col justify-center m-8 text-center">
-            <img
-              alt=""
-              className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500"
-              src="https://source.unsplash.com/100x100/?portrait?2"
-            />
-            <p className="text-xl font-semibold leading-tight">Leroy Jenkins</p>
-            <p className="dark:text-gray-600">Visual Designer</p>
-          </div>
-          <div className="flex flex-col justify-center m-8 text-center">
-            <img
-              alt=""
-              className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500"
-              src="https://source.unsplash.com/100x100/?portrait?3"
-            />
-            <p className="text-xl font-semibold leading-tight">Leroy Jenkins</p>
-            <p className="dark:text-gray-600">Visual Designer</p>
-          </div>
-          <div className="flex flex-col justify-center m-8 text-center">
-            <img
-              alt=""
-              className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500"
-              src="https://source.unsplash.com/100x100/?portrait?4"
-            />
-            <p className="text-xl font-semibold leading-tight">Leroy Jenkins</p>
-            <p className="dark:text-gray-600">Visual Designer</p>
-          </div>
-          <div className="flex flex-col justify-center m-8 text-center">
-            <img
-              alt=""
-              className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500"
-              src="https://source.unsplash.com/100x100/?portrait?5"
-            />
-            <p className="text-xl font-semibold leading-tight">Leroy Jenkins</p>
-            <p className="dark:text-gray-600">Visual Designer</p>
-          </div>
+          {team.map((team, index) => {
+            return (
+              <div
+                className="flex flex-col justify-center m-8 text-center"
+                key={index}
+              >
+                <Image
+                  src={team.picture.large}
+                  width={100}
+                  height={100}
+                  loading="lazy"
+                  className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500"
+                  alt="image"
+                />
+                <p className="text-xl font-semibold leading-tight">
+                  {" "}
+                  {team.name.last}{" "}
+                </p>
+                <p className="dark:text-gray-600">{team.email}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
